7479f30c31621dbbf904c2ed84d2fa7d62bbdd95
siddharthsham <siddhsham@gmail.com>
2020-06-22 16:39:51 +0530

    make the codesandbox fullscreen and immersive

--- website/pages/en/playground.js
+++ website/pages/en/playground.js
@@ -7,24 +7,21 @@
  * @format
  */
 
-const CompLibrary = require('../../core/CompLibrary.js');
 const React = require('react');
-const Container = CompLibrary.Container;
 
 class Playground extends React.Component {
   render() {
     return (
-      <div className="docMainWrapper wrapper">
-        <Container className="mainContainer documentContainer postContainer">
-          <div className="post">
+      // calc(100vh - 50px) is to offset the navbar height.
+      <div style={{width:"100%", height:'calc(100vh - 50px)'}}>
+          <div style={{margin: '0 0', width:"100%", height:'100%'}}>
             <iframe
               src="https://codesandbox.io/s/github/MLH-Fellowship/relay-website-example"
-              style={{width:"100%", height:500, border:0, borderRadius: 4, overflow:"hidden"}}
+              style={{width:"100%", height:'100%', border: 0, borderRadius: 0, overflow:"hidden"}}
               title="MLH-Fellowship/relay-website-example"
               allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
               sandbox="allow-autoplay allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"></iframe>
           </div>
-        </Container>
       </div>
     );
   }
