7b0e4bf95c81fcd1dd7b1f8620789e9672bf15c3
siddharthsham <siddhsham@gmail.com>
2020-08-11 19:17:58 +0530

    add URL, add CSS to test

--- website/pages/en/playground.js
+++ website/pages/en/playground.js
@@ -15,7 +15,7 @@ class Playground extends React.Component {
       // calc(100vh - 50px) is to offset the navbar height.
       <div style={{width:"100%", height:'calc(100vh - 50px)'}}>
         <iframe
-          src="https://codesandbox.io/s/github/MLH-Fellowship/relay-website-example"
+          src="https://codesandbox.io/embed/github/MLH-Fellowship/relay/tree/temp%2Ftest-codesandbox/website/relay-examples?fontsize=14&hidenavigation=1&theme=dark"
           style={{width:"100%", height:'100%', border: 0, borderRadius: 0, overflow:"hidden"}}
           title="MLH-Fellowship/relay-website-example"
           allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
